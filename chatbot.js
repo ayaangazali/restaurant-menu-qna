b0d651de12248042a90463549a824aabec35ddc3
ayaangazali <ayaangazali.work@gmail.com>
2019-07-18 12:26:22 -0700

    menu and contact rules

--- chatbot.js
+++ chatbot.js
@@ -2,7 +2,9 @@
 
 var rules = [
 	{keywords:["hours","open","close","timing","time","when"], reply:"We are open Mon-Wed 11am-9pm, Thu 11am-10pm, Fri 11am-11pm, Sat 10am-11pm and Sun 10am-8pm."},
-	{keywords:["location","address","where","directions","find"], reply:"We are located at 142 Maple Street, Riverton, OH 45011. See you soon!"}
+	{keywords:["location","address","where","directions","find"], reply:"We are located at 142 Maple Street, Riverton, OH 45011. See you soon!"},
+	{keywords:["menu","food","dishes","eat","serve"], reply:"You can see our full menu on the Menu page. We have starters, mains, desserts and drinks!"},
+	{keywords:["contact","phone","call","number","email"], reply:"You can reach us at (555) 482-1190 or email hello@thecopperspoon.com."}
 ];
 
 function sendMsg() {
